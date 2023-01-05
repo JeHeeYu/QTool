ac040ff626102e3ac9a9e8f1ac9d2fbeacdc5de6
JeHeeYu <sees1234558@gmail.com>
2023-01-05 21:56:13 +0900

    Text Init

--- QTool/Serial/serialmainwindow.cpp
+++ QTool/Serial/serialmainwindow.cpp
@@ -6,9 +6,119 @@ SerialMainWindow::SerialMainWindow(QWidget *parent) :
     ui(new Ui::SerialMainWindow)
 {
     ui->setupUi(this);
+
+    Init();
+
+    ui->textEdit->setPlaceholderText("Jehee");
 }
 
 SerialMainWindow::~SerialMainWindow()
 {
     delete ui;
+}
+
+void SerialMainWindow::Init()
+{
+    LabelInit();
+    ComboBoxInit();
+    CheckBoxInit();
+    ButtonInit();
+    RadioButtonInit();
+}
+
+void SerialMainWindow::LabelInit()
+{
+    // Port Info Area Label
+    ui->portAreaLabel->setText(PORT_AREA_LABEL);
+    ui->portNumberLabel->setText(PORT_NUMBER_LABEL);
+    ui->baudRateLabel->setText(BAUDRATE_LABEL);
+    ui->dataBitsLabel->setText(DATA_BITS_LABEL);
+    ui->stopBitsLabel->setText(STOP_BITS_LABEL);
+    ui->parityBitsLabel->setText(PARITIY_BITS_LABEL);
+
+    // Area Main Text
+    ui->sendDataAreaLabel->setText(SEND_DATA_AREA_LABEL);
+    ui->receivedSentAreaLabel->setText(RECEIVED_SENT_AREA_LABEL);
+    ui->packetAreaLabel->setText(PACKET_AREA_LABEL);
+    ui->connectionSetLabel->setText(CONNECTION_SET_AREA_LABEL);
+}
+
+void SerialMainWindow::ComboBoxInit()
+{
+    // Baud Rate Combo Box Data
+    ui->baudRateComboBox->addItem(BAUD_RATE_9600);
+    ui->baudRateComboBox->addItem(BAUD_RATE_1200);
+    ui->baudRateComboBox->addItem(BAUD_RATE_2400);
+    ui->baudRateComboBox->addItem(BAUD_RATE_4800);
+    ui->baudRateComboBox->addItem(BAUD_RATE_19200);
+    ui->baudRateComboBox->addItem(BAUD_RATE_38400);
+    ui->baudRateComboBox->addItem(BAUD_RATE_57600);
+    ui->baudRateComboBox->addItem(BAUD_RATE_115200);
+
+    // Data Bits Combo Box Data
+    ui->dataBitsComboBox->addItem(DATA_BITS_5);
+    ui->dataBitsComboBox->addItem(DATA_BITS_6);
+    ui->dataBitsComboBox->addItem(DATA_BITS_7);
+    ui->dataBitsComboBox->addItem(DATA_BITS_8);
+    ui->dataBitsComboBox->addItem(DATA_BITS_NONE);
+
+    // Stop Bits Combo Box Data
+    ui->stopBitsComboBox->addItem(STOP_BITS_1);
+    ui->stopBitsComboBox->addItem(STOP_BITS_2);
+    ui->stopBitsComboBox->addItem(STOP_BITS_3);
+    ui->stopBitsComboBox->addItem(STOP_BITS_NONE);
+
+    // Parity Bits Combo Box Data
+    ui->parityBitsComboBox->addItem(PARITY_BITS_1);
+    ui->parityBitsComboBox->addItem(PARITY_BITS_2);
+    ui->parityBitsComboBox->addItem(PARITY_BITS_3);
+    ui->parityBitsComboBox->addItem(PARITY_BITS_NONE);
+}
+
+void SerialMainWindow::CheckBoxInit()
+{
+    // Check Box Text
+    ui->dtrCheckBox->setText(DTR_CHECK_BOX_TEXT);
+    ui->dsrCheckBox->setText(DSR_CHECK_BOX_TEXT);
+    ui->rtsCheckBox->setText(RTS_CHECK_BOX_TEXT);
+    ui->ctsCheckBox->setText(CTS_CHECK_BOX_TEXT);
+
+    // Send Data Check Box Text
+    ui->firstSendDataCheckBoxASCII->setText(SEND_DATA_ASCII_TEXT);
+    ui->firstSendDataCheckBoxHEX->setText(SEND_DATA_HEX_TEXT);
+    ui->secondSendDataCheckBoxASCII->setText(SEND_DATA_ASCII_TEXT);
+    ui->secondSendDataCheckBoxHEX->setText(SEND_DATA_HEX_TEXT);
+    ui->thirdSendDataCheckBoxASCII->setText(SEND_DATA_ASCII_TEXT);
+    ui->thirdSendDataCheckBoxHEX->setText(SEND_DATA_HEX_TEXT);
+}
+
+void SerialMainWindow::ButtonInit()
+{
+    // Button Text
+    ui->serialConnectButton->setText(SERIAL_CONNECT_BUTTON_TEXT);
+    ui->serialDisconnectButton->setText(SERIAL_DISCONNECT_BUTTON_TEXT);
+    ui->saveDataButton->setText(SAVE_DATA_BUTTON_TEXT);
+    ui->searchDataButton->setText(SEARCH_DATA_BUTTON_TEXT);
+
+    // Send Data Button Text
+    ui->firstSendDataSendButton->setText(SEND_DATA_SEND_BUTTON_TEXT);
+    ui->firstSendDataClearButton->setText(SEND_DATA_CLEAR_BUTTON_TEXT);
+    ui->secondSendDataSendButton->setText(SEND_DATA_SEND_BUTTON_TEXT);
+    ui->secondSendDataClearButton->setText(SEND_DATA_CLEAR_BUTTON_TEXT);
+    ui->thirdSendDataSendButton->setText(SEND_DATA_SEND_BUTTON_TEXT);
+    ui->thirdSendDataClearButton->setText(SEND_DATA_CLEAR_BUTTON_TEXT);
+}
+
+void SerialMainWindow::RadioButtonInit()
+{
+    // Radio Button Text
+    ui->noneRadioButton->setText(NONE_RADIO_BUTTON_TEXT);
+    ui->crRadioButton->setText(CR_RADIO_BUTTON_TEXT);
+    ui->lfRadioButton->setText(LF_RADIO_BUTTON_TEXT);
+    ui->crLFRadioButton->setText(CR_LF_RADIO_BUTTON_TEXT);
+}
+
+void SerialMainWindow::TextEditInit()
+{
+
 }
--- QTool/Serial/serialmainwindow.h
+++ QTool/Serial/serialmainwindow.h
@@ -2,6 +2,73 @@
 #define SERIALMAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+
+// Label Text
+#define PORT_AREA_LABEL                 "Port"
+#define PORT_NUMBER_LABEL               "Port Number"
+#define BAUDRATE_LABEL                  "Baud Rate"
+#define DATA_BITS_LABEL                 "Data Bits"
+#define STOP_BITS_LABEL                 "Stop Bits"
+#define PARITIY_BITS_LABEL              "Parity Bits"
+#define CONNECTION_SET_AREA_LABEL       "Connection Set"
+#define SEND_DATA_AREA_LABEL            "Send Data"
+#define RECEIVED_SENT_AREA_LABEL        "Received/Send Data"
+#define PACKET_AREA_LABEL               "Packet"
+
+// Baud Rate Check Box Data
+#define BAUD_RATE_9600          "9600"
+#define BAUD_RATE_1200          "1200"
+#define BAUD_RATE_2400          "2400"
+#define BAUD_RATE_4800          "4800"
+#define BAUD_RATE_19200         "19200"
+#define BAUD_RATE_38400         "38400"
+#define BAUD_RATE_57600         "57600"
+#define BAUD_RATE_115200        "115200"
+
+// Data Bits Check Box Data
+#define DATA_BITS_5         "5"
+#define DATA_BITS_6         "6"
+#define DATA_BITS_7         "7"
+#define DATA_BITS_8         "8"
+#define DATA_BITS_NONE      "-1"
+
+// Stop Bits Check Box Data
+#define STOP_BITS_1         "1"
+#define STOP_BITS_2         "2"
+#define STOP_BITS_3         "3"
+#define STOP_BITS_NONE      "-1"
+
+// Parity Bits Check Box Data
+#define PARITY_BITS_1       "1"
+#define PARITY_BITS_2       "2"
+#define PARITY_BITS_3       "3"
+#define PARITY_BITS_NONE    "-1"
+
+// CheckBox Text
+#define DTR_CHECK_BOX_TEXT              "DTR"
+#define DSR_CHECK_BOX_TEXT              "DSR"
+#define RTS_CHECK_BOX_TEXT              "RTS"
+#define CTS_CHECK_BOX_TEXT              "CTS"
+#define SEND_DATA_ASCII_TEXT            "ASCII"
+#define SEND_DATA_HEX_TEXT              "HEX"
+
+// Button Text
+#define SERIAL_CONNECT_BUTTON_TEXT         "Serial Connect"
+#define SERIAL_DISCONNECT_BUTTON_TEXT      "Serial Disconnect"
+#define SAVE_DATA_BUTTON_TEXT              "Save Data"
+#define SEARCH_DATA_BUTTON_TEXT            "Search Data"
+#define SEND_DATA_SEND_BUTTON_TEXT         "Send"
+#define SEND_DATA_CLEAR_BUTTON_TEXT        "Clear"
+
+#define NONE_RADIO_BUTTON_TEXT          "None"
+#define CR_RADIO_BUTTON_TEXT            "CR"
+#define LF_RADIO_BUTTON_TEXT            "LF"
+#define CR_LF_RADIO_BUTTON_TEXT         "CR + LF"
+
+// Place Holder Text
+#define SEND_DATA_PLACE_HOLDER      "Enter data to send"
 
 namespace Ui {
 class SerialMainWindow;
@@ -15,8 +82,23 @@ public:
     explicit SerialMainWindow(QWidget *parent = nullptr);
     ~SerialMainWindow();
 
+public slots:
+
+public:
+
+private:
+    void Init();
+    void LabelInit();
+    void ComboBoxInit();
+    void CheckBoxInit();
+    void ButtonInit();
+    void RadioButtonInit();
+    void TextEditInit();
+
 private:
     Ui::SerialMainWindow *ui;
+
+signals:
 };
 
 #endif // SERIALMAINWINDOW_H
